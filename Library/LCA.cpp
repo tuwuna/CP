d4498d811108629e5dca0ce8c1ea9902faea3e53
tuwuna <56817415+tuwuna@users.noreply.github.com>
2020-03-04 22:31:52 +0700

    Create LCA.cpp

---
+++ Library/LCA.cpp
@@ -0,0 +1,59 @@
+struct LCA_orz {
+
+	int id = 0;
+	int h[N], tin[M], tout[M], euler[M], LOG[M];
+
+	struct node {
+		int h, v;
+		bool operator < (const node &o) const {
+			return h < o.h;
+		}
+	} mn[18][M];
+
+	void dfs(int v, int p) {
+		tin[v] = id;
+		euler[++id] = v;
+		for (auto u : adj[v]) {
+			if (h[u] == 0) {
+				h[u] = h[v] + 1;
+				dfs(u, v);
+				euler[++id] = v;
+			}
+		}
+		tout[v] = id;
+	}
+
+	void setup() {
+		for (int i = 1; i < M; ++i) {
+			LOG[i] = (i & 1) ? LOG[i - 1] : LOG[i >> 1] + 1;
+		}
+		for (int j = 0; j <= 17; ++j) {
+			for (int i = 1; i <= id; ++i) {
+				mn[j][i].h = INT_MAX;
+			}
+		}
+		dfs(1, 0);
+		memset(tin, 0x3f, sizeof tin);
+		for (int i = 1; i <= id; ++i) {
+			int x = euler[i];
+			tin[x] = min(tin[x], i);
+			tout[x] = max(tout[x], i);
+			mn[0][i] = {h[x], x};
+		}
+		for (int j = 1; j <= 17; ++j) {
+			for (int i = 1; i <= id - (1 << j) + 1; ++i) {
+				mn[j][i] = min(mn[j - 1][i], mn[j - 1][i + (1 << (j - 1))]);
+			}
+		}
+	}
+
+	int get_min(int l, int r) {
+		int k = LOG[r - l + 1];
+		return min(mn[k][l], mn[k][r - (1 << k) + 1]).v;
+	}
+
+} LCA;
+ 
+int get_lca(int u, int v) {
+	return LCA.get_min(min(LCA.tin[u], LCA.tin[v]), max(LCA.tout[u], LCA.tout[v]));
+}
