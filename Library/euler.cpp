3bc6bd66e5a70da4f67b9c2b7a5ab7ae0c48840a
tuwuna <56817415+tuwuna@users.noreply.github.com>
2020-03-05 12:10:29 +0700

    Update euler.cpp

--- Library/euler.cpp
+++ Library/euler.cpp
@@ -1,12 +1,13 @@
 struct euler_orz {
 
-	int id, tin[M], tout[M];
+	int id, tin[N], tout[N];
 
 	void dfs(int v, int p) {
 		tin[v] = ++id;
 		for (auto u : adj[v]) {
 			if (u != p) {
 				dfs(u, v);
+				child[v].push_back(u);
 			}
 		}
 		tout[v] = ++id;
@@ -14,6 +15,16 @@ struct euler_orz {
 
 	void setup() {
 		dfs(1, 0);
+	}
+
+	bool contain(int v, int u) {
+		return tin[v] < tin[u] && tout[u] < tout[v];
+	}
+
+	int child_in_simple_path(int v, int u) {
+		return *--upper_bound(child[v].begin(), child[v].end(), u, [this](int u, int v) {
+			return tin[u] < tin[v];
+		});
 	}
 
 	void update(int v, int x) {
