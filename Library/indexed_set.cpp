66b57cb45b467717eb7e12690aeaed642aa80785
tuwuna <56817415+tuwuna@users.noreply.github.com>
2020-03-16 08:53:46 +0700

    Update indexed_set.cpp

--- Library/indexed_set.cpp
+++ Library/indexed_set.cpp
@@ -6,8 +6,9 @@ using namespace __gnu_pbds;
 typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> indexed_set;
 // end of indexed_set
 
-or
 
+
+// 
 struct indexed_set {
 	
 	int sz = 0, bit[N];
@@ -29,6 +30,49 @@ struct indexed_set {
 		for (int j = 17; j >= 0; --j) {
 			ans += 1 << j;
 			if (ans < N && sum + bit[ans] < k) {
+				sum += bit[ans];
+			} else {
+				ans -= 1 << j;
+			}
+		}
+		return ans + 1;
+	}
+	
+	int order_of_key(int k) {
+		int ans = 0;
+		while (k >= 1) {
+			ans += bit[k];
+			k -= k & -k;
+		}
+		return ans;
+	}
+	
+} is;
+
+// log ^ 2
+
+const MAX = 1e9;
+
+struct indexed_set {
+	
+	map<int, int> bit;
+	
+	indexed_set() {
+		bit.clear();	
+	}
+
+	void update(int k, int x) {
+		while (k <= MAX) {
+			bit[k] += x;
+			k += k & -k;
+		}
+	}
+
+	int find_by_order(int k) {
+		int ans = 0, sum = 0;
+		for (int j = log2(MAX); j >= 0; --j) {
+			ans += 1 << j;
+			if (ans < MAX && sum + bit[ans] < k) {
 				sum += bit[ans];
 			} else {
 				ans -= 1 << j;
