d83e28a7a70c012a2c2563b5facd2d703a766a4f
tuwuna <56817415+tuwuna@users.noreply.github.com>
2020-03-16 08:27:38 +0700

    Update indexed_set.cpp

--- Library/indexed_set.cpp
+++ Library/indexed_set.cpp
@@ -1,3 +1,13 @@
+// begin of indexed_set
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/detail/standard_policies.hpp>
+using namespace __gnu_pbds;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> indexed_set;
+// end of indexed_set
+
+or
+
 struct indexed_set {
 	
 	int sz = 0, bit[N];
